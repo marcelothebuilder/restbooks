fbd44b89162b503ee2cc82a9803885319b385439
Marcelo Paixão Resende <marcelofoxes@gmail.com>
2016-07-05 09:10:51 -0300

    refactor inline mapper code into an external class

--- src/main/java/io/github/marcelothebuilder/restbooks/repository/sql/LivrosImpl.java
+++ src/main/java/io/github/marcelothebuilder/restbooks/repository/sql/LivrosImpl.java
@@ -9,13 +9,12 @@ import java.util.List;
 import java.util.Set;
 
 import org.jooq.DSLContext;
-import org.jooq.Record;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import io.github.marcelothebuilder.restbooks.domain.Comentario;
 import io.github.marcelothebuilder.restbooks.domain.Livro;
 import io.github.marcelothebuilder.restbooks.repository.Livros;
+import io.github.marcelothebuilder.restbooks.repository.sql.mapper.LivroMapper;
 
 @Repository
 public class LivrosImpl implements Livros {
@@ -33,52 +32,9 @@ public class LivrosImpl implements Livros {
 			.from(LIVRO)
 			.join(COMENTARIO)
 			.on(COMENTARIO.CODIGO_LIVRO.eq(LIVRO.CODIGO))
-			.fetch(	r -> {
-				Record livroRecord = r.into(LIVRO.fields());
-				Record comentarioRecord = r.into(COMENTARIO.fields());
-				
-				Livro livro = livroRecord.into(Livro.class);
-				Comentario comentario = comentarioRecord.into(Comentario.class);
-				
-				livros.add(livro);
-				
-				// get fresh livro from set
-				Livro freshLivro = livros.stream().filter(l -> l.equals(livro)).findFirst().get();
-				
-				freshLivro.getComentarios().add(comentario);
-				
-				return null;
-			});
+			.fetch(new LivroMapper(livros));
 		
 		return new ArrayList<>(livros);
 	}
-
-//
-//	public List<Livro> todos2() {
-//		List<Livro> livros = dsl.select(
-//				LIVRO.CODIGO,
-//				LIVRO.NOME,
-//				LIVRO.PUBLICACAO,
-//				LIVRO.EDITORA,
-//				LIVRO.RESUMO,
-//				LIVRO.AUTOR)
-//			.from(LIVRO)
-//			.fetchInto(Livro.class);
-//		
-//		livros.stream().forEach(livro -> {
-//			livro.setComentarios(
-//					dsl.select(
-//							COMENTARIO.CODIGO,
-//							COMENTARIO.CODIGO_LIVRO,
-//							COMENTARIO.AUTOR,
-//							COMENTARIO.CONTEUDO
-//						)
-//						.from(COMENTARIO).fetchInto(Comentario.class)
-//					);
-//			
-//		});
-//		
-//		return livros;
-//	}
 
 }
---
+++ src/main/java/io/github/marcelothebuilder/restbooks/repository/sql/mapper/LivroMapper.java
@@ -0,0 +1,60 @@
+package io.github.marcelothebuilder.restbooks.repository.sql.mapper;
+
+import static io.github.marcelothebuilder.restbooks.jooq.tables.Comentario.COMENTARIO;
+import static io.github.marcelothebuilder.restbooks.jooq.tables.Livro.LIVRO;
+
+import java.util.Set;
+
+import org.jooq.Record;
+import org.jooq.RecordMapper;
+
+import io.github.marcelothebuilder.restbooks.domain.Comentario;
+import io.github.marcelothebuilder.restbooks.domain.Livro;
+
+/**
+ * <p>
+ * Recebe um record proveniente de um join com todas tabelas-filha de
+ * {@link LIVRO} e retorna um objeto {@link Livro} com as tabelas-filha
+ * inseridas como objetos DTO.
+ * </p>
+ * 
+ * <p>
+ * Mapeia records de {@link COMENTARIO} como uma coleção em ScheduledDTO, e para
+ * isso armazenamos os Livro em um Set.
+ * </p>
+ * 
+ * <p>
+ * TODO: Esta classe viola o contrato de {@link RecordMapper} ao armazenar os
+ * resultados num Set. O ideal é que esta classe não mantenha referências de
+ * listas iniciadas externamente para funcionar.
+ * </p>
+ * 
+ * @author Marcelo Paixao Resende
+ *
+ */
+public class LivroMapper implements RecordMapper<Record, Livro> {
+	private Set<Livro> livros;
+
+	public LivroMapper(Set<Livro> livros) {
+		this.livros = livros;
+	}
+	
+	@Override
+	public Livro map(Record record) {
+		Record livroRecord = record.into(LIVRO.fields());
+		Record comentarioRecord = record.into(COMENTARIO.fields());
+		
+		Livro livro = livroRecord.into(Livro.class);
+		Comentario comentario = comentarioRecord.into(Comentario.class);
+		
+		this.livros.add(livro);
+		
+		// get fresh livro from set
+		Livro freshLivro = livros.stream().filter(l -> l.equals(livro)).findFirst().get();
+		
+		freshLivro.getComentarios().add(comentario);
+		
+		return livro;
+	}
+
+}
