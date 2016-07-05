a6b2fdea9a08a4b331a2f49dbbd383806c6790ac
Marcelo Paixão Resende <marcelofoxes@gmail.com>
2016-07-05 12:23:52 -0300

    add @Repository to Livros interface

--- src/main/java/io/github/marcelothebuilder/restbooks/repository/Livros.java
+++ src/main/java/io/github/marcelothebuilder/restbooks/repository/Livros.java
@@ -2,8 +2,11 @@ package io.github.marcelothebuilder.restbooks.repository;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import io.github.marcelothebuilder.restbooks.domain.Livro;
 
+@Repository
 public interface Livros {
 	List<Livro> todos();
 	Livro salvar(Livro livro);
