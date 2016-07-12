f4d0140277788fa59cc09a5033f0c8f0a236d6fd
Marcelo Paixão Resende <marcelofoxes@gmail.com>
2016-07-12 13:44:47 -0300

    controller accepts/consumes xml and json

--- src/main/java/io/github/marcelothebuilder/restbooks/controllers/LivrosController.java
+++ src/main/java/io/github/marcelothebuilder/restbooks/controllers/LivrosController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,7 +29,10 @@ import io.github.marcelothebuilder.restbooks.service.exceptions.LivroInexistente
  *
  */
 @RestController
-@RequestMapping("/livros")
+@RequestMapping(
+		path = "/livros",
+		produces = { MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.APPLICATION_XML_VALUE },
+		consumes = { MediaType.APPLICATION_JSON_UTF8_VALUE, MediaType.APPLICATION_XML_VALUE })
 public class LivrosController {
 
 	@Autowired
